61b1657e5275e0e5690461c1ba06596ca2c3c190
Amy Cao <Amy.369@hotmail.com>
2022-11-29 18:14:31 -0800

    merged covid-cases cleaning into master

--- data_processing.py
+++ data_processing.py
@@ -1,6 +1,33 @@
 import pandas as pd
 
-# Population_count cleaning
+
+# -- cleaning and getting data from covid-19-case-count.csv
+
+def month_to_quarter(month):
+    if 1 <= month <= 3:
+        return 1
+    elif 4 <= month <= 6:
+        return 2
+    elif 7 <= month <= 9:
+        return 3
+    else:
+        return 4
+
+
+input_cases = pd.read_csv("inputs/covid-19-case-count.csv")
+
+# remove rows where province name is Canada and Repatriated travellers
+updated = input_cases['update'] == 1
+cases = input_cases[updated]
+
+cases.rename(columns={"prname": "province", "reporting_year": "year"}, inplace=True)
+cases['month'] = pd.DatetimeIndex(cases['date']).month
+cases['quarter'] = cases.apply(lambda x: month_to_quarter(x.month), axis=1)
+cleaned_cases = cases[['province', 'quarter', 'year', 'totalcases', 'numtotal_last7', 'numdeaths', 'numdeaths_last7']]
+
+
+# -- cleaning and getting data from population-count.csv
+
 # RETURN: population columns = [province/GEO, quarter, year, population]
 data = pd.read_csv('../353-project-covid-predictions/inputs/population_count.csv', index_col='REF_DATE').reset_index()
 
@@ -17,4 +44,3 @@ pop_df = data.pivot(index='GEO', columns='REF_DATE', values='VALUE').reset_index
 pop_df.columns = ['GEOGRAPHY', '2020-Q1', '2020-Q2', '2020-Q3', '2020-Q4', '2021-Q1', '2021-Q2', '2021Q3', '2021Q4',
                   '2022-Q1', '2022-Q2', '2022-Q3']
 
-print(pop_df)
